34deeed863d50ed64f31c542625ff08ce89e7151
yxy <null>
2023-05-11 21:41:54 +0800

    addmysql

---
+++ MysqlEF/HouseConfig.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace MysqlEF
+{
+    public class HouseConfig:IEntityTypeConfiguration<House>
+    {
+        public void Configure(EntityTypeBuilder<House> builder)
+        {
+            builder.ToTable("T_House");
+            builder.Property(b => b.Name).IsRequired();
+        }
+    }
+}
